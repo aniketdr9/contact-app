53a22c7dc27783a6336d47192e5670bd2f886d62
Aniket Rathod <57947853+aniketdr9@users.noreply.github.com>
2024-01-16 19:34:43 +0530

    add contack functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { uuid } from 'uuidv4';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
+
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      "name": "Aniket",
-      "email": "aniket@test.com"
-    },
-    {
-      id: "2",
-      "name": "Andy",
-      "email": "text@test.com"
-    }
-  ]
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, {id: uuid(), ...contacts}]);
+  }
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)));
+    if(retriveContacts)
+      setContacts(retriveContacts);
+  }, [contacts]);
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={ addContactHandler }/>
       <ContactList contacts={ contacts } />
     </div>
   );
